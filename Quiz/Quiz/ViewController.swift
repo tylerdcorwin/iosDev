b7fbd820dc0186272e11e85b6801068443ec1878
Corwin <001131100@N101-B5-Mac.local>
2018-01-30 18:54:11 -0500

    chapter 1 tutorial

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Corwin, Tyler D on 1/30/18.
+//  Copyright © 2018 Corwin, Tyler D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+    "What is 7+7?",
+    "What is the capital of Vermont?",
+    "What is congnac made from?"
+    ]
+    
+    let answers: [String] = [
+    "14",
+    "Montpelier",
+    "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+            questionLabel.text = question
+            answerLabel.text = "????"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
